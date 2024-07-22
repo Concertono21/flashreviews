054427154c894e9541203cc438f8e79ac966230b
Alexis Ducros <concerto_no21@Alexis.local>
2024-07-22 14:41:01 +0200

    Update environment variables and next-auth configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 export default {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'http://localhost:3000',
+    NEXT_PUBLIC_BASE_URL: 'https://flashreviews-j8un272ai-concertono21s-projects.vercel.app',
     MAILGUN_API_KEY: '51356527-c6d8b73f',
     MAILGUN_DOMAIN: 'sandboxd862468b60cf4f58bd5705371f28eda4.mailgun.org',
   },
