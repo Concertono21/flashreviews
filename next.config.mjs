77f3bb9d1d36c28304f7ba82477f6a797000bfa1
Alexis Ducros <concerto_no21@Alexis.local>
2024-07-22 14:44:00 +0200

    Fix CSP and debug React errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@
 export default {
   env: {
     NEXT_PUBLIC_BASE_URL: 'https://flashreviews-j8un272ai-concertono21s-projects.vercel.app',
-    MAILGUN_API_KEY: '51356527-c6d8b73f',
-    MAILGUN_DOMAIN: 'sandboxd862468b60cf4f58bd5705371f28eda4.mailgun.org',
+    MAILGUN_API_KEY: 'your_mailgun_api_key',
+    MAILGUN_DOMAIN: 'your_mailgun_domain',
   },
   async headers() {
     return [
