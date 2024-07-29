3f1a8f075c4d911be5e7f37db0c468ebd5a6e413
Alexis Ducros <concerto_no21@Alexis.local>
2024-07-29 14:55:00 +0200

    Update CSP and use environment variables for embed.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 export default {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'https://flashreviews-j8un272ai-concertono21s-projects.vercel.app',
+    NEXT_PUBLIC_BASE_URL: 'https://flashreviews.vercel.app',
     MAILGUN_API_KEY: 'your_mailgun_api_key',
     MAILGUN_DOMAIN: 'your_mailgun_domain',
   },
@@ -11,7 +11,7 @@ export default {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://flashreviews-j8un272ai-concertono21s-projects.vercel.app; object-src 'none';",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://flashreviews.vercel.app; object-src 'none';",
           },
         ],
       },
