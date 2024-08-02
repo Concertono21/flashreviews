d21ce485863162bfc6d279a50553028c96eb25c6
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-02 20:23:23 +0200

    Update

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,21 @@
 const Hero = () => {
   return (
     <section id="home" className="hero bg-black py-20 text-center">
-      <div className="hero-content">
-        <h1 className="text-4xl font-bold">Turn Visitors into Engaged Customers with Quick Review Popups</h1>
-        <p className="text-xl mt-4">Capture valuable feedback instantly with engaging pop-up reviews, enhancing your customer experience and driving growth.</p>
-        <ul className="benefits list-disc mt-4">
+      <div className="hero-content mx-auto px-4 max-w-screen-lg">
+        <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl">Turn Visitors into Engaged Customers with Quick Review Popups</h1>
+        <p className="text-xl mt-4 sm:text-2xl md:text-3xl">Capture valuable feedback instantly with engaging pop-up reviews, enhancing your customer experience and driving growth.</p>
+        <ul className="benefits list-disc mt-4 text-left mx-auto max-w-md sm:text-lg md:text-xl">
           <li>Pay once, use forever</li>
           <li>1-minute no-code setup</li>
           <li>Boost customer satisfaction</li>
         </ul>
-        <a href="signin.html" className="cta-button text-black px-6 py-3 rounded mt-6 inline-block"style={{ backgroundColor: '#bafd00' }} >Get Flash Reviews</a>
+        <a
+          href="signin.html"
+          className="cta-button text-black px-6 py-3 rounded mt-6 inline-block"
+          style={{ backgroundColor: '#bafd00' }}
+        >
+          Get Flash Reviews
+        </a>
       </div>
     </section>
   );
