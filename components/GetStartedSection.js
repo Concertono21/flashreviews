784809bd8274284f265391afc575d89e59cd5404
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-02 20:35:02 +0200

    Update

--- components/GetStartedSection.js
+++ components/GetStartedSection.js
@@ -1,4 +1,3 @@
-// components/GetStartedSection.js
 "use client";
 
 import { useRouter } from 'next/navigation';
@@ -8,8 +7,15 @@ const GetStartedSection = () => {
 
   return (
     <section id="get-started" className="py-20 text-center bg-black">
-      <h1 className="text-3xl font-bold">Get Flash Review</h1>
-      <button onClick={() => router.push('/login')} className="bg-[#bafd00] hover:bg-[#bbdd11] text-black hover:text-white px-6 py-3 rounded mt-6">Get Started</button>
+      <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-3xl font-bold text-white sm:text-4xl md:text-5xl">Get Flash Review</h1>
+        <button 
+          onClick={() => router.push('/login')} 
+          className="bg-[#bafd00] hover:bg-[#bbdd11] text-black hover:text-white px-6 py-3 rounded mt-6 text-xl sm:text-2xl md:text-3xl"
+        >
+          Get Started
+        </button>
+      </div>
     </section>
   );
 };
