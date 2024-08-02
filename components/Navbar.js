b1ab159803d6a0e649d13888348e84d66ef1fb3a
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-02 18:54:33 +0200

    Update

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     <div className="w-full bg-black text-white">
       <nav className="flex items-center justify-between p-6 max-w-6xl mx-auto">
         <div className="flex items-center">
-          <Image src="/logo.png" alt="Logo" width={80} height={40} className="h-10 w-20 object-contain" />
+          <Image src="/logo.png" alt="Logo" width={160} height={80} className="h-20 w-40 object-contain" />
         </div>
         <ul className="flex space-x-6">
           <li>
