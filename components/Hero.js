fded9e79a8475c745743016ae37f6b3ad71ad408
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-27 15:30:48 +0200

    Update

--- components/Hero.js
+++ components/Hero.js
@@ -4,33 +4,43 @@ const Hero = () => {
   return (
     <section id="home" className="hero bg-black py-20 text-center">
       <div className="hero-content">
-        <h1 className="text-4xl font-bold" style={{ fontWeight: 600 }}>
+        <h1 className="text-7xl font-bold" style={{ fontWeight: 600 }}>
           <span
             style={{
-              background: 'linear-gradient(to top, #000000, #ffffff)', // Gradient from black to white
+              background: 'linear-gradient(to top, #969696, #ffffff)',
               WebkitBackgroundClip: 'text',
               color: 'transparent',
               fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
             }}
           >
-            Turn Visitors into Engaged Customers
+            Turn Visitors into Customers
           </span>
           <br />
           <span
             style={{
-              background: 'linear-gradient(to top, #00ff00, #39ff14)', // Gradient from green to neon green
+              background: 'linear-gradient(to top, #969696, #ffffff)',
               WebkitBackgroundClip: 'text',
               color: 'transparent',
               fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
             }}
           >
-            with Quick Review Popups
+            Deliver What Matters Most
           </span>
         </h1>
-        <p className="text-xl mt-4" style={{ color: '#FFFFFF' }}> {/* White text color */}
+        <p
+          className="text-xl mt-4 text-white"
+          style={{
+            fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
+          }}
+        >
           Capture valuable feedback instantly with engaging pop-up reviews, enhancing your customer experience and driving growth.
         </p>
-        <ul className="benefits list-disc mt-4" style={{ color: '#FFFFFF' }}> {/* White text color */}
+        <ul
+          className="benefits list-disc mt-4 text-white"
+          style={{
+            fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
+          }}
+        >
           <li>Pay once, use forever</li>
           <li>1-minute no-code setup</li>
           <li>Boost customer satisfaction</li>
@@ -38,7 +48,7 @@ const Hero = () => {
         <Link href="/login">
           <a
             className="cta-button text-black px-6 py-3 rounded mt-6 inline-block"
-            style={{ backgroundColor: '#bafd00' }} // Bright green background for the button
+            style={{ backgroundColor: '#bafd00' }}
           >
             Get Flash Reviews
           </a>
