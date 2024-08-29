eded7a5e5b1841a562e1857fc39c8cb1889da0bb
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-29 10:25:55 +0200

    Update

--- components/PopupHistory.js
+++ components/PopupHistory.js
@@ -106,14 +106,10 @@ const PopupHistory = ({ handleDeletePopup, websites = [], refreshData }) => {
           filteredPopups.map((popup) => (
             <div
               key={popup._id}
-              className="bg-[#1C1C1E] p-4 rounded-lg shadow-md mb-4"
+              className="bg-[#1C1C1E] p-4 rounded-lg shadow-md mb-4 relative"
               style={{
-                position: 'relative',
                 backgroundColor: styleSettings[popup.style]?.backgroundColor || '#fff',
                 color: styleSettings[popup.style]?.color || '#000',
-                display: 'flex',
-                flexDirection: 'column',
-                textAlign: 'left',
               }}
             >
               <button
@@ -145,79 +141,39 @@ const PopupHistory = ({ handleDeletePopup, websites = [], refreshData }) => {
                   <p className="text-xs" style={{ color: styleSettings[popup.style]?.color || 'black' }}>
                     <FaClock className="inline-block mr-1" />{popup.timing} seconds
                   </p>
-                  <p className="text-xs" style={{ color: styleSettings[popup.style]?.color || 'black' }}>
+                  <p className="text-xs mb-2" style={{ color: styleSettings[popup.style]?.color || 'black' }}>
                     <FaGlobe className="inline-block mr-1" />{popup.website}
                   </p>
+                  {popup.enableStars && (
+                    <div className="flex justify-start">
+                      {[1, 2, 3, 4, 5].map((star) => (
+                        <svg
+                          key={star}
+                          className="w-4 h-4"
+                          fill={popup.enableStars ? 'gold' : 'gray'}
+                          viewBox="0 0 24 24"
+                          stroke="none"
+                          style={{
+                            fontSize: '12px',
+                            color: popup.enableStars ? 'gold' : 'grey',
+                            marginRight: '5px',
+                          }}
+                        >
+                          <path
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+                          />
+                        </svg>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </div>
-              {popup.enableStars && (
-                <div className="flex justify-start mb-2">
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <svg
-                      key={star}
-                      className="w-4 h-4"
-                      fill={popup.enableStars ? 'gold' : 'gray'}
-                      viewBox="0 0 24 24"
-                      stroke="none"
-                      style={{
-                        fontSize: '12px',
-                        color: popup.enableStars ? 'gold' : 'grey',
-                        marginRight: '5px',
-                      }}
-                    >
-                      <path
-                        d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-                      />
-                    </svg>
-                  ))}
-                </div>
-              )}
             </div>
           ))
         ) : (
           <p className="text-[#F0F0F3]">No popups created yet.</p>
         )}
       </div>
-      <div className="bg-[#2C2C2E] border border-[#3A3A3C] p-4 rounded-lg mt-4 text-[#F0F0F3]">
-        <h3 className="text-lg font-bold mb-2">Make your FlashReviews live 🎉</h3>
-        <p>Paste this snippet in the &lt;head&gt; of your website.</p>
-      </div>
-      <button
-        className="bg-[#1C1C1E] text-[#F0F0F3] border border-[#3A3A3C] px-4 py-2 rounded-full w-full mt-4"
-        onClick={handleGenerateCode}
-      >
-        Generate Code
-      </button>
-      {generatedCode && (
-        <div
-          className="bg-[#2C2C2E] border border-[#3A3A3C] p-4 rounded-lg mt-4 text-[#F0F0F3] relative"
-          style={{ wordWrap: 'break-word' }}
-        >
-          <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word', color: '#F0F0F3', marginRight: '30px' }}>
-            {generatedCode}
-          </pre>
-          <button
-            onClick={handleCopyCode}
-            style={{
-              position: 'absolute',
-              top: '10px',
-              right: '10px',
-              background: 'none',
-              border: 'none',
-              cursor: 'pointer',
-              padding: 0,
-              margin: 0,
-            }}
-          >
-            <Image
-              src="/copy-icon.png" // Ensure this file is in the public folder
-              alt="Copy Icon"
-              width={24}
-              height={24}
-            />
-          </button>
-        </div>
-      )}
     </div>
   );
 };
