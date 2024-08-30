0e675210eb9ff5aa6f4fd3b3ab256cb6c9ea5b6b
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-30 17:48:18 +0200

    Update

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
     <div>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://flashreviews.co/embed.js" data-website="https://www.flashreviews.co"></script>
+        <script src="https://flashreviews.co/embed.js" data-website="flashreviews.co"></script>
       </Head>
       <Navbar />
       <Hero />
