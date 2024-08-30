af39ac91e722bfe3f72dbc34ec768049b70ae317
Alexis Ducros <concerto_no21@Alexis.local>
2024-08-30 11:35:55 +0200

    Update

--- components/PreviewPopup.js
+++ components/PreviewPopup.js
@@ -82,7 +82,7 @@ const PreviewPopup = ({ popupSettings, handleClose }) => {
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              marginRight: '10px', // Space between logo and text
+              marginRight: '5px', // Space between logo and text
             }}
           >
             <Image
@@ -101,8 +101,8 @@ const PreviewPopup = ({ popupSettings, handleClose }) => {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'flex-start',
-            maxWidth: popupSettings.logo ? 'calc(100% - 60px)' : 'calc(100% - 20px)', // Adjust width based on logo presence
-            paddingRight: '20px',
+            maxWidth: popupSettings.logo ? 'calc(100% - 60px)' : 'calc(100% - 15px)', // Adjust width based on logo presence
+            paddingRight: '15px',
           }}
         >
           <div
